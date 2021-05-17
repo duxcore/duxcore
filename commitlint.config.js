982e971d8e68e9ff6b4ef728f7ca46741f167951
Jackson <jrobe3221@gmail.com>
2021-05-17 18:44:06 -0500

    feat(global): monorepo go brr
    no

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+				"api",
+				"website",
+				"wrapper"
+      ],
+    ],
+  },
+};
