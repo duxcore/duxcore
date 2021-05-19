d2c4d8b212473448ebd850b067c5345eb9e3eaa1
ajmnz <arnaujim@gmail.com>
2021-05-19 13:33:16 +0200

    feat(backend): add mongodb driver and set up connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,24 @@
-export class App {}
+import { MongoClient } from "mongodb";
+
+require("dotenv").config();
+
+export class App {
+  client!: MongoClient;
+
+  async start() {
+    this.client = new MongoClient(process.env.MONGO_URI!);
+    try {
+      await this.client.connect();
+
+      await this.client.db("backend").command({ ping: 1 });
+      console.log("Connected to db");
+    } finally {
+      // This will close conn once finished/errored
+      await this.client.close();
+    }
+  }
+}
 
 const app = new App();
+
+app.start().catch(console.dir);
