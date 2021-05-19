c5ba28cadecde36d4e0bc9e4e2469ad16f46d1fd
Reuben Tier <reuben.tier@icloud.com>
2021-05-19 12:23:01 +0100

    refactor(backend): remove typeorm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,3 @@
-import { createConnection, Connection } from "typeorm";
-
-export class App {
-  connection!: Connection | undefined;
-
-  async start() {
-    this.connection = await createConnection();
-  }
-}
+export class App {}
 
 const app = new App();
-app.start();
