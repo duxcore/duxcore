70f2a71b02ecbc326ca8b92067587facab933f3f
HoloPanio <jrobe3221@gmail.com>
2021-06-24 21:44:53 -0500

    feat(backend): added user registration route

--- backend/src/api/routes/user/users.ts
+++ backend/src/api/routes/user/users.ts
@@ -1,5 +1,6 @@
 import { newApiResponse } from "../../../helpers/newApiResponse";
 import { sendApiResponse } from "../../../helpers/sendApiResponse";
+import { createUser } from "../../../lib/users";
 import { ApiRoute } from "../../../types/api";
 import { validateCaptcha } from "../../middleware/validateCaptcha";
 
@@ -8,6 +9,32 @@ export const users: ApiRoute[] = [
     route: "/user",
     method: "post",
     middleware: [validateCaptcha],
-    executor: (req, res) => {},
+    executor: (req, res) => {
+      createUser(req.body.data)
+        .then((user) => {
+          sendApiResponse(
+            res,
+            newApiResponse({
+              status: 201,
+              message: "User created successfully!",
+              successful: true,
+            })
+          );
+        })
+        .catch((err) => {
+          sendApiResponse(
+            res,
+            newApiResponse({
+              status: 500,
+              message:
+                "An error has occured whilst attempting to create a user",
+              data: {
+                error: err.message,
+              },
+              successful: false,
+            })
+          );
+        });
+    },
   },
 ];
