5619447798872b87a646e4fa13734c9267e6170f
Jackson Roberts <jrobe3221@gmail.com>
2021-11-01 11:02:17 -0500

    chore: clean up project and migrate dashboard

--- frontend/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.scss";
-import type { AppProps } from "next/app";
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-export default App;
