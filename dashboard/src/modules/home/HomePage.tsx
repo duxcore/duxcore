52628aa26c1d1572b5dcf30844eebd349358a130
ajmnz <arnaujim@gmail.com>
2021-11-02 12:40:59 +0100

    feat(dashboard): add logout button

--- dashboard/src/modules/home/HomePage.tsx
+++ dashboard/src/modules/home/HomePage.tsx
@@ -5,9 +5,19 @@ import { useAuth } from "../auth/useAuth";
 interface HomePageProps {}
 
 export const HomePage: PageComponent<HomePageProps> = () => {
-  const { user } = useAuth();
+  const { user, logOut } = useAuth();
 
-  return <>{user ? <div>Hey {user.firstName}</div> : <div>loading</div>}</>;
+  return (
+    <div>
+      <div>Hey {user?.firstName}</div>
+      <button
+        onClick={() => logOut()}
+        className="py-0.5 mt-5 ml-3 px-2 bg-gray-800 hover:bg-gray-700 rounded-5"
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 HomePage.requiresAuth = true;
