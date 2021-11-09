8af64128a89e95c0b6ae02eabe7a9866212cb012
Jackson Roberts <jrobe3221@gmail.com>
2021-11-09 10:33:31 -0600

    feat(core): added comments, added updateEmail, and added updatePassword to UserManager class

--- core/src/classes/UserManager.ts
+++ core/src/classes/UserManager.ts
@@ -1,5 +1,6 @@
 import { User } from ".prisma/client";
 import { prismaInstance } from "../../prisma/instance";
+import Password from "./Password";
 
 export default class UserManager {
 
@@ -18,6 +19,16 @@ export default class UserManager {
   get firstName(): string { return this._raw.firstName; }
   get lastName(): string { return this._raw.lastName; }
 
+  /**
+   * Revoke All User Refresh Tokens
+   * 
+   * This method will revoke all refresh tokens under this specific user.
+   * Once revoked, they will no longer be capible of using an existing refresh
+   * token to generate a new authentication token, thus requiring them to login
+   * again.
+   * 
+   * @returns - User Object
+   */
   async revokeAllRefreshTokens() {
     await prismaInstance.userRefreshToken.deleteMany({
       where: {
@@ -27,6 +38,56 @@ export default class UserManager {
     return;
   }
 
+  /**
+   * Update User Email Address.
+   * 
+   * @param email - The new email you'd like to apply to this user.
+   * @returns - Updated user Object
+   */
+  async updateEmail(email: string): Promise<this> {
+    await prismaInstance.user.update({
+      data: {
+        email
+      },
+      where: {
+        id: this.id
+      }
+    });
+
+    /**
+     * Update Email logic to send verification emails and stuff here in future
+     */
+
+    this._raw.email = email;
+    return this;
+  }
+
+  /**
+   * Update User Password
+   * 
+   * @param password - The new password to be applied to user (raw text)
+   * @returns - Updated User Object
+   */
+  async updatePassword(password: string): Promise<this> {
+    const newHash = Password.hash(password);
+
+    await prismaInstance.user.update({
+      data: {
+        password: newHash
+      },
+      where: {
+        id: this.id
+      }
+    });
+
+    this._raw.password = newHash;
+    return this;
+  }
+
+  /**
+   * Get plain user object.
+   * @returns - JSON Object reflecting user data.
+   */
   toJson() {
     return {
       id: this.id,
