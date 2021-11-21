4dd3bfd007105a018957a2f0d6d69c351a38b81e
GoldyyDev <55450464+goldyydev@users.noreply.github.com>
2021-11-21 14:09:13 +0200

    feat(dashboard): added a header (+ accesibility)
    Added a header (/navbar) and and made the website accessible
    
    closes #37

---
+++ dashboard/src/components/forms/DropdownButton.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement, useState } from "react";
+import Link from "next/link";
+
+interface DropdownButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick?: () => void;
+  href?: string;
+  icon?: JSX.Element;
+}
+
+export const DropdownButton: React.FC<DropdownButtonProps> = ({
+  onClick = () => undefined,
+  href = "#",
+  icon = <></>,
+  children,
+}) => {
+  return (
+    <Link href={href} passHref={true}>
+      <button className="w-full hover:bg-gray-700 p-1 transition-all font-medium items-center text-md text-left" onClick={onClick}>
+        <span style={{ verticalAlign: "baseline" }}>
+          <span style={{ fontSize: "1.5rem" }} className="mr-1">
+            {icon}
+          </span>
+          {children}
+        </span>
+      </button>
+    </Link>
+  );
+};
