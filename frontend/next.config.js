1d94c3a150a1cd7fb3070aa67b6763c24eb8bfc7
Amitoj Singh <amitojsingh366@gmail.com>
2022-01-14 13:00:01 +0300

    feat(frontend): add redirects for discord and github

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/vZTbxBNSVA',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/duxcore',
+        permanent: true,
+      },
+    ]
+  },
 };
