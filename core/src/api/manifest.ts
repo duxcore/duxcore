687df3d6c56e6e0291736bf94555981080fe489c
Jackson Roberts <jrobe3221@gmail.com>
2022-06-06 10:00:17 -0500

    chore(core): i honestly don't remember what I was working on

--- core/src/api/manifest.ts
+++ core/src/api/manifest.ts
@@ -8,7 +8,6 @@ import { authRoutes } from "./routes/auth";
 import { resetEmail } from "./routes/resetEmail";
 import { servicesRouter } from "./routes/services/services";
 import { usersRouter } from "./routes/users/users";
-import { monitorRouter } from "./routes/monitors/monitors";
 import { daemonRoutes } from "./routes/daemons/daemons";
 import { projectsRouter } from "./routes/projects/projects";
 
@@ -19,13 +18,7 @@ export const apiManifest = manifestation.newManifest({
       version: 1,
       middleware: [apiLimiter, cors()],
       routes: [teapot, apiStats, ...authRoutes, resetEmail],
-      routers: [
-        servicesRouter,
-        usersRouter,
-        monitorRouter,
-        daemonRoutes,
-        projectsRouter,
-      ],
+      routers: [servicesRouter, usersRouter, daemonRoutes, projectsRouter],
     },
   ],
 });
--- core/src/api/routes/monitors/monitors.ts
+++
@@ -1,8 +0,0 @@
-import { manifestation } from "@duxcore/manifestation";
-import { serverMonitorsRouter } from "./servers/servers";
-
-export const monitorRouter = manifestation.newRouter({
-  route: "/monitors",
-  routes: [],
-  routers: [serverMonitorsRouter]
-});
