86ad44c03e35e33d852831ca76de8512c20922c5
= <otterlord.dev@gmail.com>
2022-08-09 07:56:30 +0100

    fix(core): fix get route for service types
    fixed incorrectly spelt .toJson call & added typing to prevent this happening again

--- core/src/api/routes/services/types.ts
+++ core/src/api/routes/services/types.ts
@@ -1,4 +1,5 @@
 import { manifestation } from "@duxcore/manifestation";
+import ServiceTypeManager from "../../../classes/ServiceTypeManager";
 import { apiError, errorConstructor } from "../../../helpers/apiError";
 import { sendApiErrors } from "../../../helpers/sendApiErrors";
 import { services } from "../../../lib/services";
@@ -11,25 +12,25 @@ export const serviceTypesRouter = manifestation.newRouter({
       route: "/",
       method: "get",
       executor: async (req, res) => {
-        let data;
+        let data: ServiceTypeManager[] = [];
 
 				const errors = apiError.createErrorStack();
 
         try {
             data = await services.types.fetchAll();
         } catch (e) {
-					errors.append(errorConstructor.internalServerError(e as Error))
+					errors.append(errorConstructor.internalServerError(e as Error));
 				}
 
-				if (errors.stack.length > 0) return sendApiErrors(res, ...errors.stack)
+				if (errors.stack.length > 0) return sendApiErrors(res, ...errors.stack);
 
         return manifestation.sendApiResponse(res, {
             status: 200,
-            message: "Successfully fetched projects!",
-            data: data.map(d => d.toJSON()),
+            message: "Successfully fetched service types!",
+            data: data.map(d => d.toJson()),
             successful: true,
           });
       }
     })
   ]
-})
+});
